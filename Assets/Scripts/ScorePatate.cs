4246ce596e19ffd4542e36709c180985da9680bd
unknown <samuellalibel@hotmail.com>
2016-10-09 09:22:58 -0400

    Calcul du score en cours.

---
+++ Assets/Scripts/ScorePatate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScorePatate : MonoBehaviour {
+
+    public float[] ScoreTeam1 = new float[4];
+    public float[] ScoreTeam2 = new float[4];
+    public float[] GodFavor = new float[4];
+
+    public static ScorePatate instance = null;
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static ScorePatate Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+    }
+}
